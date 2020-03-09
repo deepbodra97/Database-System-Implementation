3d170d2975cf060f9f3d62318007cf938c9fa64a
Deep Bodra <deepbodra96@gmail.com>
2020-03-09 12:45:20 -0400

    P3 init:copy from P2.2

---
+++ P3: Relational Operators/src/BigQ.cc
@@ -0,0 +1,262 @@
+#include "BigQ.h"
+
+// A wrapper to be passed to the thread for 'Two Pass Multiway Merge Sort Thread'
+class BigQMemberHolder{
+
+	public:
+		Pipe *in; //input pipe
+		Pipe *out; // output pipe
+		OrderMaker *sortorder; // ordermaker
+		int runVectorlen; // run length: in pages
+		// File file; // file instance
+
+		BigQMemberHolder(Pipe &in, Pipe &out, OrderMaker &sortorder, int runVectorlen){
+			this->in = &in;
+			this->out = &out;
+			this->sortorder = &sortorder;
+			this->runVectorlen = runVectorlen;
+		}
+};
+
+// comparator class for Record
+class RecordComparator{
+	ComparisonEngine comparisonEngine; // comparison engine
+	OrderMaker *sortorder; // sort order
+
+	public:
+		RecordComparator(OrderMaker sortorder){
+			this->sortorder = &sortorder;
+		}
+
+		bool operator()(Record *left, Record *right){
+			return comparisonEngine.Compare(left, right, sortorder)<0;
+		}
+};
+
+// Instances of this class will be stored in the priority queue for phase 2 of 'Two Pass Multiway Merge Sort Thread'
+class RunRecord{
+	public:
+		Record record; // record
+		int runNumber; // run number of the record
+};
+
+// comparator class for RunRecord
+class RunRecordComparator{
+	ComparisonEngine comparisonEngine;
+	OrderMaker *sortorder;
+
+	public:
+		RunRecordComparator(OrderMaker sortorder){
+			this->sortorder = &sortorder;
+		}
+
+		bool operator()(RunRecord *left, RunRecord *right){
+			return comparisonEngine.Compare(&(left->record), &(right->record), sortorder)>=0;
+		}
+};
+
+// To track the information about the buffers in the phase 2 of 'Two Pass Multiway Merge Sort Thread'
+class RunRecordMetaData{
+	public:
+		Page page; // page
+		int currentPageNumber; // current page number
+		int endPageNumber; // last page number
+		int runLength; // length of this run in the number of pages
+};
+
+// 'Two Pass Multiway Merge Sort' Thread
+void *TwoPassMultiwayMergeSort (void *arg) {
+
+	/* PHASE 1: Split phase
+	 * read data from in pipe sort them into runVectorlen pages
+	 */
+
+	BigQMemberHolder *bigQMemberHolder;
+	bigQMemberHolder = (BigQMemberHolder*)arg;
+
+	Record recordFromPipe; // record from input pipe
+
+	File runFile; // file to store runs
+	Page runPage; // page to store records for a run
+	vector<Record*> runVector; // vector to sort records in memory
+
+	Page tempPage; // temp page used to count 'runLength' records
+	
+	RecordComparator recordComparator = RecordComparator(*bigQMemberHolder->sortorder); // comparator
+
+	runFile.Open(0, "./runFile.bin"); // open run file
+
+	int nPagesFilledForARun = 0; // number of pages filled for the current run
+
+
+	vector<RunRecordMetaData*> runRecordMetaData; // acts as a buffer for the merge phase. new records will be  picked from this and added to the priority queue
+	int nRuns = 0;
+
+	while(bigQMemberHolder->in->Remove(&recordFromPipe)){ // while there are records in the pipe
+		Record *copyOfRecordFromPipe = new Record(); // to store copy of the record
+		copyOfRecordFromPipe->Copy(&recordFromPipe); // copy the record from pipe
+
+		if(tempPage.Append(&recordFromPipe) == 1){ // record fits in the temp page
+			runVector.push_back(copyOfRecordFromPipe); // add the record in the vector for the current run
+		} else{ // record does not fit in the page
+			nPagesFilledForARun++; // increase page number
+			tempPage.EmptyItOut(); // empty the temp page
+			if(nPagesFilledForARun == bigQMemberHolder->runVectorlen){ // if runVectorlen pages of records processed
+				int currentRunLength = 0;
+
+				stable_sort(runVector.begin(), runVector.end(), recordComparator); // sort the records
+				for(std::vector<Record*>::iterator it=runVector.begin(); it!=runVector.end(); ++it){ // commit the sorted records to the file
+					if(runPage.Append(*it) == 0){
+						runFile.AddPage(&runPage, runFile.GetLength()-1<0?0:runFile.GetLength()-1);
+						runPage.EmptyItOut();
+						runPage.Append(*it);
+						currentRunLength++;
+					}
+				}
+
+				if(runPage.GetNumRecs()!=0){ // if the page is not full but still has records then also we must commit it to the file
+					runFile.AddPage(&runPage, runFile.GetLength()-1<0?0:runFile.GetLength()-1);
+					runPage.EmptyItOut();
+					currentRunLength++;
+				}
+				RunRecordMetaData *newRunRecordMetaDatum = new RunRecordMetaData;
+				if(nRuns == 0){
+					newRunRecordMetaDatum->currentPageNumber = 0;
+					newRunRecordMetaDatum->endPageNumber = currentRunLength-1;
+				} else{
+					newRunRecordMetaDatum->currentPageNumber = runRecordMetaData[nRuns-1]->endPageNumber+1;
+					newRunRecordMetaDatum->endPageNumber = newRunRecordMetaDatum->currentPageNumber+currentRunLength-1;
+				}
+				runRecordMetaData.push_back(newRunRecordMetaDatum);
+
+				nRuns++;
+				nPagesFilledForARun = 0; // set the number of pages filled to 0 for the next run
+				runVector.clear(); // clear vector
+			}
+			tempPage.Append(&recordFromPipe);
+			runVector.push_back(copyOfRecordFromPipe); // add the record that did not fit earlier as a part of the next run
+		}
+	}
+
+	nPagesFilledForARun = 0;
+	runPage.EmptyItOut();
+	tempPage.EmptyItOut();
+	if(!runVector.empty()){
+		stable_sort(runVector.begin(), runVector.end(), recordComparator); // sort the records
+
+		int currentRunLength = 0;
+		for(std::vector<Record*>::iterator it=runVector.begin(); it!=runVector.end(); ++it){ // commit the sorted records to the file
+			if(runPage.Append(*it) == 0){
+				runFile.AddPage(&runPage, runFile.GetLength()-1<0?0:runFile.GetLength()-1);
+				runPage.EmptyItOut();
+				runPage.Append(*it);
+				currentRunLength++;
+			}
+		}
+
+		if(runPage.GetNumRecs()!=0){ // if the page is not full but still has records then also we must commit it to the file
+			runFile.AddPage(&runPage, runFile.GetLength()-1<0?0:runFile.GetLength()-1);
+			runPage.EmptyItOut();
+			currentRunLength++;
+		}
+
+		RunRecordMetaData *newRunRecordMetaDatum = new RunRecordMetaData;
+		if(nRuns == 0){
+			newRunRecordMetaDatum->currentPageNumber = 0;
+			newRunRecordMetaDatum->endPageNumber = currentRunLength-1;
+		} else{
+			newRunRecordMetaDatum->currentPageNumber = runRecordMetaData[nRuns-1]->endPageNumber+1;
+			newRunRecordMetaDatum->endPageNumber = newRunRecordMetaDatum->currentPageNumber+currentRunLength-1;
+		}
+		runRecordMetaData.push_back(newRunRecordMetaDatum);
+
+		// free the Record objects lying around in the memory
+		for(vector<Record *>::iterator it=runVector.begin();it!=runVector.end();++it){
+	        delete *it;
+	    }
+	    nRuns++;
+		runVector.clear(); // free the space occupied by the vector
+	}
+
+
+	nPagesFilledForARun = 0;
+	if(!runVector.empty()){
+		stable_sort(runVector.begin(), runVector.end(), recordComparator); // sort the records
+
+		for(std::vector<Record*>::iterator it=runVector.begin(); it!=runVector.end(); ++it){ // commit the sorted records to the file
+			bigQMemberHolder->out->Insert(*it);
+		}
+		// free the Record objects lying around in the memory
+		for(vector<Record *>::iterator it=runVector.begin();it!=runVector.end();++it){
+	        delete *it;
+	    }
+		runVector.clear(); // free the space occupied by the vector
+	}
+	
+
+
+	runFile.Close();
+
+
+	/* PHASE 2: Merge sorted Runs
+	 * construct priority queue over sorted runVectors and dump sorted data into the out pipe
+	 */
+	runFile.Open(1, "./runFile.bin");
+	int nPages = runFile.GetLength()-1;
+	priority_queue<RunRecord*, vector<RunRecord*>, RunRecordComparator> priorityQueue(*bigQMemberHolder->sortorder); // priority queue to merge the records
+
+	if(nPages==0){
+		cout<<"RunFile is empty! There are no pages in the runFile"<<endl;
+	}else{
+
+		// init runRecordMetaData
+		for(int i=0, startPageNumber=0; i<nRuns; i++, startPageNumber+=bigQMemberHolder->runVectorlen){
+			runFile.GetPage(&runRecordMetaData[i]->page, runRecordMetaData[i]->currentPageNumber); // get the 1st page of i-th run
+			RunRecord *runRecord = new RunRecord; // this record will be added to the priority queue
+			runRecord->runNumber = i; // set the run number
+			runRecordMetaData[i]->page.GetFirst(&(runRecord->record)); // get the first record from the page
+			priorityQueue.push(runRecord); // push the record to the priority queue
+		}
+		
+		while(!priorityQueue.empty()){ // while the queue has runrecords
+			RunRecord *outputRunRecord = priorityQueue.top(); // get a pointer to the smallest runrecord in the queue
+			priorityQueue.pop(); // remove the runrecord from the queue
+			bigQMemberHolder->out->Insert(&(outputRunRecord->record)); // insert in the output pipe
+			RunRecord *newRunRecord = new RunRecord; // the record after the popped record in the run will be stored in this
+			newRunRecord->runNumber = outputRunRecord->runNumber; // set run number of this record equal to that of the popped record
+			if(runRecordMetaData[outputRunRecord->runNumber]->page.GetFirst(&(newRunRecord->record)) == 1){ // if this page for this run has more records then fetch the 1st record
+				
+				priorityQueue.push(newRunRecord); // push this record to the queue
+			} else{ // if the page doesn't have more records
+				runRecordMetaData[outputRunRecord->runNumber]->currentPageNumber += 1; // increase the current page number by 1
+				if(runRecordMetaData[outputRunRecord->runNumber]->currentPageNumber <= runRecordMetaData[outputRunRecord->runNumber]->endPageNumber){ // if we are not past the last page of the run
+					runFile.GetPage(&runRecordMetaData[outputRunRecord->runNumber]->page, runRecordMetaData[outputRunRecord->runNumber]->currentPageNumber); // get the next page
+					runRecordMetaData[outputRunRecord->runNumber]->page.GetFirst(&(newRunRecord->record)); // get the 1st record from this page
+					priorityQueue.push(newRunRecord); // push it into the queue
+				}
+			}
+		}
+		
+	}
+	for(vector<RunRecordMetaData*>::iterator it=runRecordMetaData.begin();it!=runRecordMetaData.end();++it){
+        delete *it;
+    }
+	runRecordMetaData.clear(); // free the space occupied by the vector
+
+	runFile.Close(); // close runFile
+	// remove("./runFile.bin"); // remove runFile
+
+	// finally shut down the out pipe
+	bigQMemberHolder->out->ShutDown();
+}
+
+
+BigQ :: BigQ (Pipe &in, Pipe &out, OrderMaker &sortorder, int runVectorlen) {
+	BigQMemberHolder *bigQMemberHolder = new BigQMemberHolder(in, out, sortorder, runVectorlen);
+		
+	pthread_t threadTwoPassMultiwayMergeSort;
+	pthread_create(&threadTwoPassMultiwayMergeSort, NULL, &TwoPassMultiwayMergeSort, (void*)bigQMemberHolder);
+}
+
+BigQ::~BigQ () {
+}
---
+++ P3: Relational Operators/src/gtest_sortedfile.cc
@@ -0,0 +1,220 @@
+#include "DBFile.h"
+#include "BigQ.h"
+#include "test.h"
+#include <fstream>
+#include <gtest/gtest.h>
+
+
+TEST(CreateTest, ReturnValueTest){
+	DBFile dbfile;
+	ASSERT_EQ(1, dbfile.Create((char *)"./sortedfile.bin", sorted, NULL)); // check if the return value of Create() is 1 or not
+}
+
+TEST(MoveFirstTest, RecordEqualityTest){
+	setup ();
+
+	relation *rel_ptr[] = {n, r, c, p, ps, s, o, li};
+
+	int tindx = 1;
+	int findx = 0;
+	while (findx < 1 || findx > 8) {
+		cout << "\n select table: \n";
+		cout << "\t 1. nation \n";
+		cout << "\t 2. region \n";
+		cout << "\t 3. customer \n";
+		cout << "\t 4. part \n";
+		cout << "\t 5. partsupp \n";
+		cout << "\t 6. supplier \n";
+		cout << "\t 7. orders \n";
+		cout << "\t 8. lineitem \n \t ";
+		cin >> findx;
+	}
+	rel = rel_ptr [findx - 1];
+
+	OrderMaker o;
+	rel->get_sort_order (o);
+
+	int runlen = 0;
+	while (runlen < 1) {
+		cout << "\t\n specify runlength:\n\t ";
+		cin >> runlen;
+	}
+	struct {OrderMaker *o; int l;} startup = {&o, runlen};
+
+	DBFile dbfile;
+	cout << "\n output to dbfile : " << rel->path () << endl;
+	dbfile.Create (rel->path(), sorted, &startup);
+	dbfile.Close ();
+
+	char tbl_path[100];
+	sprintf (tbl_path, "%s%s.tbl", tpch_dir, rel->name()); 
+	cout << " input from file : " << tbl_path << endl;
+
+    FILE *tblfile = fopen (tbl_path, "r");
+
+
+	dbfile.Open (rel->path ());
+	Record temp;
+
+	Record firstRecordOne; // 1st record after MoveFirst and before closing
+	Record firstRecordTwo; // 1st record after closing after MoveFirst
+
+	int counter=0;
+	while (temp.SuckNextRecord (rel->schema (), tblfile)) {
+		dbfile.Add (temp);
+		counter++;
+	}
+	cout << "\n create finished.. " << counter << " recs inserted\n";
+	dbfile.MoveFirst(); // move first
+	dbfile.GetNext(firstRecordOne); // copy the record from ptrCurrentRecord into firstRecordFetched
+	dbfile.Close();
+
+	dbfile.Open(rel->path());
+	dbfile.MoveFirst(); // move first
+	dbfile.GetNext(firstRecordTwo); // copy the record from ptrCurrentRecord into firstRecordFetched
+
+	ComparisonEngine comparisonEngine;
+	int comparisonResult = comparisonEngine.Compare(&firstRecordOne, &firstRecordTwo, &o); // compare the 2 records
+
+	ASSERT_EQ(0, comparisonResult); // the result should be '0' as the records are both the same
+
+	dbfile.Close ();
+	fclose (tblfile);
+}
+
+
+// After the *.bin.bigq file has been generated, this function checks if the intermediate run file was generated or not
+// The run file has the name "yolo.runfile"
+TEST(BigQTest, RunFileExistenceTest){		
+	ifstream runFile("./yolo.runfile");
+	ASSERT_TRUE(runFile.good())<<"The intermediate runfile wasn't created"; // assert: does the intermediate run file exist?
+	cleanup ();
+}
+
+TEST(BigQTest, RunFileSizeTest){
+	setup ();
+
+	relation *rel_ptr[] = {n, r, c, p, ps, o, li};
+
+	int findx = 0;
+	while (findx < 1 || findx > 7) {
+		cout << "\n select bigq file to test: (*.bin.bigq)\n";
+		cout << "\t 1. nation \n";
+		cout << "\t 2. region \n";
+		cout << "\t 3. customer \n";
+		cout << "\t 4. part \n";
+		cout << "\t 5. partsupp \n";
+		cout << "\t 6. orders \n";
+		cout << "\t 7. lineitem \n \t ";
+		cin >> findx;
+	}
+	rel = rel_ptr [findx - 1];	
+	ifstream binFile(rel->path());
+	ifstream runFile("./yolo.runfile");
+
+	ASSERT_TRUE(runFile.tellg() >= binFile.tellg())<<"The size of the runFile is not as atleast as that of the binFile"; // assert: does the intermediate run file exist?
+	cleanup ();
+}
+
+
+// After the *.bin.bigq file has been generated, this function checks if the number of records in the *.bin file
+// are the same as that in "yolo.runfile"
+TEST(BigQTest, NumberOfRecordsTest){
+	setup ();
+
+	relation *rel_ptr[] = {n, r, c, p, ps, o, li};
+
+	int findx = 0;
+	while (findx < 1 || findx > 7) {
+		cout << "\n select bigq file to test: (*.bin.bigq)\n";
+		cout << "\t 1. nation \n";
+		cout << "\t 2. region \n";
+		cout << "\t 3. customer \n";
+		cout << "\t 4. part \n";
+		cout << "\t 5. partsupp \n";
+		cout << "\t 6. orders \n";
+		cout << "\t 7. lineitem \n \t ";
+		cin >> findx;
+	}
+	rel = rel_ptr [findx - 1];
+
+	Record temp;
+
+	DBFile dbfile;
+	dbfile.Open(rel->path());
+	dbfile.MoveFirst();
+	int nBinFile = 0;
+	while(dbfile.GetNext(temp)){
+		nBinFile++;
+	}
+	dbfile.Close();
+
+	char bigQFileName[100];
+	sprintf (bigQFileName, "%s", rel->path ());
+	dbfile.Open(bigQFileName);
+	dbfile.MoveFirst();
+	int nBigQFile = 0;
+	while(dbfile.GetNext(temp)){
+		nBigQFile++;
+	}
+	dbfile.Close();
+
+	ASSERT_EQ(nBinFile, nBigQFile)<<"Number of input records is not equal to that of output records"; // assert: the number of errors should be 0
+	cleanup ();
+}
+
+// After the *.bin.bigq file has been generated, this function checks if the number of records that are
+// out of order is 0 or not
+TEST(BigQTest, SortOrderTest){
+	setup ();
+	relation *rel_ptr[] = {n, r, c, p, ps, o, li};
+
+	int findx = 0;
+	while (findx < 1 || findx > 7) {
+		cout << "\n select bigq file to test: (*.bin.bigq)\n";
+		cout << "\t 1. nation \n";
+		cout << "\t 2. region \n";
+		cout << "\t 3. customer \n";
+		cout << "\t 4. part \n";
+		cout << "\t 5. partsupp \n";
+		cout << "\t 6. orders \n";
+		cout << "\t 7. lineitem \n \t ";
+		cin >> findx;
+	}
+	rel = rel_ptr [findx - 1];
+
+	OrderMaker sortorder;
+	rel->get_sort_order (sortorder);
+
+	DBFile dbfile;
+	char bigFileName[100];
+	sprintf (bigFileName, "%s", rel->path ());
+	dbfile.Open(bigFileName);
+
+	ComparisonEngine comparisonEngine;
+	Record rec[2];
+	Record *last = NULL, *prev = NULL;
+	int i = 0;
+	int nErrors = 0;
+	dbfile.MoveFirst();
+	while (dbfile.GetNext(rec[i%2]) == 1) {
+		prev = last;
+		last = &rec[i%2];
+
+		if (prev && last) {
+			if (comparisonEngine.Compare (prev, last, &sortorder) == 1) { // are prev and last out of order?
+				nErrors++;
+			}
+		}
+		i++;
+	}
+
+	ASSERT_EQ(nErrors, 0)<<nErrors<<" recs not in sorted order"; // assert: the number of errors should be 0
+	cleanup ();
+}
+
+
+int main(int argc, char **argv){
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
